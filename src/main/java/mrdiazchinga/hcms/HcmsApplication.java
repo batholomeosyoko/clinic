38aae530cb93e2c7ef1294892e45e2667ea91af1
batholomeo syoko <batholomeosyoko@gmail.com>
2026-09-01 20:04:59 +0300

    style: format main method indentation for consistency

--- src/main/java/mrdiazchinga/hcms/HcmsApplication.java
+++ src/main/java/mrdiazchinga/hcms/HcmsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HcmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HcmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HcmsApplication.class, args);
+    }
 
 }
