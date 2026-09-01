77a155b4e2f724312786c9bf1e1f1736583c2c0c
batholomeo syoko <batholomeosyoko@gmail.com>
2026-09-01 20:06:44 +0300

    style: consolidate annotations for email field in UpdateUserDto

--- src/main/java/mrdiazchinga/hcms/user/dto/UpdateUserDto.java
+++ src/main/java/mrdiazchinga/hcms/user/dto/UpdateUserDto.java
@@ -5,8 +5,7 @@ import jakarta.validation.constraints.Size;
 import mrdiazchinga.hcms.user.entity.Role;
 
 public record UpdateUserDto(
-        @Email 
-        @Size(min = 3, max = 254) String email,
+        @Email @Size(min = 3, max = 254) String email,
         @Size(min = 8, max = 72) String password,
         Role role,
         Boolean enabled
