b6d34d138d4ef96571833c358da2544c0447f1a3
batholomeo syoko <batholomeosyoko@gmail.com>
2026-09-01 19:33:12 +0300

    feat: add HcmsApplicationTests class for application context loading tests

---
+++ src/test/java/mrdiazchinga/hcms/HcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package mrdiazchinga.hcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
