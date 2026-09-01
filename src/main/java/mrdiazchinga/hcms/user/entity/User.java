f91106e01fca95826584d55b57d9db67a64b1720
batholomeo syoko <batholomeosyoko@gmail.com>
2026-09-01 20:07:15 +0300

    style: reorganize import statements for consistency in User entity

--- src/main/java/mrdiazchinga/hcms/user/entity/User.java
+++ src/main/java/mrdiazchinga/hcms/user/entity/User.java
@@ -1,9 +1,21 @@
 package mrdiazchinga.hcms.user.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-
 import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
