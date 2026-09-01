a123ccf990a890d655ddcc1dc819386134d96b83
batholomeo syoko <batholomeosyoko@gmail.com>
2026-09-01 20:06:17 +0300

    style: reorder import statements for improved clarity in UserRepository

--- src/main/java/mrdiazchinga/hcms/user/UserRepository.java
+++ src/main/java/mrdiazchinga/hcms/user/UserRepository.java
@@ -2,9 +2,8 @@ package mrdiazchinga.hcms.user;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import mrdiazchinga.hcms.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
